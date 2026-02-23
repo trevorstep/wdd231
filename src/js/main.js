cea9e9b17b15f1813c0fd699c32a296ac5a86415
Trevor Stephenson <trevors19themail@gmail.com>
2026-02-23 01:28:38 -0700

    added js for opening menu, and styled some stuff and installed  lint

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,21 @@ import { getParkData, parkInfoLinks, getInfoLinks } from "./parkService.mjs";
 import setHeaderFooter from "./setHeaderFooter.mjs";
 import { mediaCardTemplate } from "./templates.mjs";
 
+
+function Navigation(){
+const menuButton =document.querySelector("#global-nav-toggle");
+const nav = document.querySelector(".global-nav");
+
+if (!menuButton || !nav) return;
+  menuButton.addEventListener("click", () => {
+  nav.classList.toggle("show");
+  const isOpen = nav.classList.contains("show");
+  
+  menuButton.setAttribute("aria-expanded", isOpen);
+  menuButton.setAttribute("aria-label", isOpen ? "Close Menu" : "Open Menu");
+});
+}
+
 function setParkIntro(parkData) {
   const introEl = document.querySelector(".intro");
   introEl.innerHTML = `<h1>${parkData.fullName}</h1>
@@ -18,8 +33,9 @@ function setParkInfoLinks(parkData) {
 
 async function init() {
   const parkData = await getParkData();
-  const links = getInfoLinks(parkData.images);
-  setHeaderFooter(parkData);
+  // const links = getInfoLinks(parkData.images);
+  await setHeaderFooter(parkData);
+  Navigation();
   setParkIntro(parkData);
   setParkInfoLinks(parkInfoLinks);
 }
